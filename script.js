d16f00fb9d5e76efc6f4b5a129142b734759c0f4
h4ck3rtr4d3r <86321646+h4ck3rtr4d3r@users.noreply.github.com>
2022-05-18 08:59:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,160 @@
+/*Bash*/
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
+
+open.addEventListener("click", () => {
+  modal_container.classList.add("show");
+});
+
+close.addEventListener("click", () => {
+  modal_container.classList.remove("show");
+});
+
+/*HTML*/
+const openHtml = document.getElementById("open_html");
+const modalContainerHtml = document.getElementById("modal_container_html");
+const closeHtml = document.getElementById("close_html");
+
+openHtml.addEventListener("click", () => {
+  modalContainerHtml.classList.add("show");
+});
+
+closeHtml.addEventListener("click", () => {
+  modalContainerHtml.classList.remove("show");
+});
+
+/*CSS*/
+const openCss = document.getElementById("open_css");
+const modalContainerCss = document.getElementById("modal_container_css");
+const closeCss = document.getElementById("close_css");
+
+openCss.addEventListener("click", () => {
+  modalContainerCss.classList.add("show");
+});
+
+closeCss.addEventListener("click", () => {
+  modalContainerCss.classList.remove("show");
+});
+
+/*JS*/
+const openJs = document.getElementById("open_js");
+const modalContainerJs = document.getElementById("modal_container_js");
+const closeJs = document.getElementById("close_js");
+
+openJs.addEventListener("click", () => {
+  modalContainerJs.classList.add("show");
+});
+
+closeJs.addEventListener("click", () => {
+  modalContainerJs.classList.remove("show");
+});
+
+/*Python*/
+const openPython = document.getElementById("open_python");
+const modalContainerPython = document.getElementById("modal_container_python");
+const closePython = document.getElementById("close_python");
+
+openPython.addEventListener("click", () => {
+  modalContainerPython.classList.add("show");
+});
+
+closePython.addEventListener("click", () => {
+  modalContainerPython.classList.remove("show");
+});
+
+/*Solidity*/
+const openSolidity = document.getElementById("open_solidity");
+const modalContainerSolidity = document.getElementById(
+  "modal_container_solidity"
+);
+const closeSolidity = document.getElementById("close_solidity");
+
+openSolidity.addEventListener("click", () => {
+  modalContainerSolidity.classList.add("show");
+});
+
+closeSolidity.addEventListener("click", () => {
+  modalContainerSolidity.classList.remove("show");
+});
+
+/*NextJs*/
+const openNext = document.getElementById("open_next");
+const modalContainerNext = document.getElementById("modal_container_next");
+const closeNext = document.getElementById("close_next");
+
+openNext.addEventListener("click", () => {
+  modalContainerNext.classList.add("show");
+});
+
+closeNext.addEventListener("click", () => {
+  modalContainerNext.classList.remove("show");
+});
+
+/*GitHub*/
+const openGit = document.getElementById("open_git");
+const modalContainerGit = document.getElementById("modal_container_git");
+const closeGit = document.getElementById("close_git");
+
+openGit.addEventListener("click", () => {
+  modalContainerGit.classList.add("show");
+});
+
+closeGit.addEventListener("click", () => {
+  modalContainerGit.classList.remove("show");
+});
+
+/*NodeJS*/
+const openNode = document.getElementById("open_node");
+const modalContainerNode = document.getElementById("modal_container_node");
+const closeNode = document.getElementById("close_node");
+
+openNode.addEventListener("click", () => {
+  modalContainerNode.classList.add("show");
+});
+
+closeNode.addEventListener("click", () => {
+  modalContainerNode.classList.remove("show");
+});
+
+/*Docker*/
+const openDocker = document.getElementById("open_docker");
+const modalContainerDocker = document.getElementById("modal_container_docker");
+const closeDocker = document.getElementById("close_docker");
+
+openDocker.addEventListener("click", () => {
+  modalContainerDocker.classList.add("show");
+});
+
+closeDocker.addEventListener("click", () => {
+  modalContainerDocker.classList.remove("show");
+});
+
+/*Kubernetes*/
+const openKubernetes = document.getElementById("open_kubernetes");
+const modalContainerKubernetes = document.getElementById("modal_container_kubernetes");
+const closeKubernetes = document.getElementById("close_kubernetes");
+
+openKubernetes.addEventListener("click", () => {
+  modalContainerKubernetes.classList.add("show");
+});
+
+closeKubernetes.addEventListener("click", () => {
+  modalContainerKubernetes.classList.remove("show");
+});
+
+/*Bitcoin*/
+const openBtc = document.getElementById("open_btc");
+const modalContainerBtc = document.getElementById("modal_container_btc");
+const closeBtc = document.getElementById("close_btc");
+
+openBtc.addEventListener("click", () => {
+  modalContainerBtc.classList.add("show");
+});
+
+closeBtc.addEventListener("click", () => {
+  modalContainerBtc.classList.remove("show");
+});
+
+
+
